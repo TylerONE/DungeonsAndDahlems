5ff590565cbf104180c4360476589dc825f022ad
TylerONE <yorukagekitsune@gmail.com>
2017-11-16 10:21:02 -0700

    Update gameMechanics.js

--- gameMechanics.js
+++ gameMechanics.js
@@ -1,6 +1,24 @@
-window.onload = function() {
-  var canvas = document.getElementById("gameScreen");
-  var ctx = canvas.getContext("2d");
-  var img = document.getElementById("mainBackground.gif");
-  ctx.drawImage(img, 100, 100);
-  };
+window.onload = startGame();
+function startGame() {
+    myGameArea.start();
+    myGamePiece = new character(30, 30, "red", 10, 120);
+}
+
+var myGameArea = {
+    canvas : document.createElement("gameScreen"),
+    start : function() {
+        this.canvas.width = 1500;
+        this.canvas.height = 500;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    }
+}
+function character(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y; 
+    ctx = myGameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+}
