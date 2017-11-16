b7e205850b3fca1dd56487e0a88dcd38db8fd965
TylerONE <yorukagekitsune@gmail.com>
2017-11-16 10:00:01 -0700

    Create gameMechanics.js

---
+++ gameMechanics.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+  var canvas = document.getElementById("gameScreen");
+  var ctx = canvas.getContext("2d");
+  var img = document.getElementById("mainBackground.gif");
+  ctx.drawImage(img, 100, 100);
+  };
