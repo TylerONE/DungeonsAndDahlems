9b4f3baac71f2378ba462ea9e6752d76a483050b
TylerONE <yorukagekitsune@gmail.com>
2017-11-17 15:21:13 -0700

    Update gameMechanics.js

--- gameMechanics.js
+++ gameMechanics.js
@@ -13,7 +13,7 @@ const context = canvas.getContext('2d');
 
 context.fillRect(0,0,50,50);
 
-loadImage('DungeonsAndDahlems/gameBase/tileset.png')
+loadImage('gameBase/tileset.png')
 .then(image => {
     context.drawImage(image,0, 0);
 });
