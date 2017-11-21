96b330db23bbba2add7a0555c842080cae1d2800
TylerONE <yorukagekitsune@gmail.com>
2017-11-21 10:14:46 -0700

    Update gameMechanics.js

--- gameMechanics.js
+++ gameMechanics.js
@@ -44,7 +44,7 @@ const context = canvas.getContext('2d');
 
 loadImage('gameBase/tileset.png')
 .then(image => {
-    const sprites = new SpriteSheet(image, 32, 32);
+    const sprites = new SpriteSheet(image, 16, 16);
     sprites.define('ground', 0, 0);
     sprites.draw('ground',context, 45, 62);
  
