9a4aa006d980a8398117917d9e9a676ba97f491f
TylerONE <yorukagekitsune@gmail.com>
2017-11-22 09:37:52 -0700

    Update gameMechanics.js

--- gameMechanics.js
+++ gameMechanics.js
@@ -8,40 +8,6 @@ function loadImage(url){
  });
 }
 
-class SpriteSheet{
- constructor(image, width, height){
-  this.image = image;
-  this.width = width;
-  this.height = height;
-  this.tiles = new Map();
-  }
-  define(name, x, y){
-    const buffer = document.createElement('canvas');
-    buffer.width = this.width;
-    buffer.height = this.height;
-    buffer
-        .getContext('2d')
-        .drawImage(
-        this.image,
-           x * this.width,
-           y * this.height,
-           this.width,
-           this.height,
-           0,
-           0,
-           this.width,
-           this.height);
-   this.tiles.set(name, buffer);
-  }
- draw(name, context, x, y){
-   const buffer = this.tiles.get(name);
-  context.drawImage(buffer, x, y);
- }
-}
-
-const canvas = document.getElementById('gameScreen');
-const context = canvas.getContext('2d');
-
 loadImage('gameBase/tileset.png')
 .then(image => {
     const sprites = new SpriteSheet(image, 32, 32);
