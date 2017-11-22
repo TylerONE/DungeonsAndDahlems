0ebe09acb653098218e001fb1b11d35b35102d48
TylerONE <yorukagekitsune@gmail.com>
2017-11-22 09:57:42 -0700

    Update gameMechanics.js

--- gameMechanics.js
+++ gameMechanics.js
@@ -8,5 +8,6 @@ loadImage('gameBase/tileset.png')
 .then(image => {
     const sprites = new SpriteSheet(image, 32, 32);
     sprites.define('ground', 0, 0);
-    sprites.draw('ground',context, 45, 62);
+    sprites.define('sky', 96, 655);
+    sprites.draw('sky',context, 45, 62);
 });
