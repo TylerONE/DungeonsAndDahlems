60d673787688e68b341485375746f3c798f42876
TylerONE <yorukagekitsune@gmail.com>
2017-11-27 09:41:06 -0700

    Update gameMechanics.js

--- gameMechanics.js
+++ gameMechanics.js
@@ -8,7 +8,7 @@ loadImage('gameBase/tileset.png')
 .then(image => {
     const sprites = new SpriteSheet(image, 32, 32);
     sprites.define('ground', 0, 0);
-    sprites.define('sky', 2.98, 22.89);
+    sprites.define('sky', 2.92, 22.89);
     
     for(let x = 0; x < 47; ++x) {
         for(let y = 0; y < 16; ++y){
