9513ca3ee62b02dd28721f839f0469411eb65fa8
TylerONE <yorukagekitsune@gmail.com>
2017-12-05 10:09:09 -0700

    Create sprites.js

---
+++ sprites.js
@@ -0,0 +1,19 @@
+export function loadDahlemSprite() {
+    return loadImage('gameBase/Character.png')
+    .then(image => {
+        const sprites = new SpriteSheet(image, 32, 32);
+        sprites.define('idle', 259, 1, 16, 32);
+        return sprites;
+    });
+}
+
+export function loadBackgroundSprites() {
+    return loadImage('gameBase/tileset.png')
+    .then(image => {
+        const sprites = new SpriteSheet(image, 32, 32);
+        sprites.defineTile('upperGround', 0, 0);
+        sprites.defineTile('lowerGround', 0, 1);
+        sprites.defineTile('sky', 2.92, 22.89);
+        return sprites;
+    });
+}
