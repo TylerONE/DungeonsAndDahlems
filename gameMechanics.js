0e658bf76da51dd2e16acb6e87e43f0480902e1f
TylerONE <yorukagekitsune@gmail.com>
2017-12-05 10:40:22 -0700

    Update gameMechanics.js

--- gameMechanics.js
+++ gameMechanics.js
@@ -15,19 +15,43 @@ function drawBackground(background, context, sprites) {
 const canvas = document.getElementById('gameScreen');
 const context = canvas.getContext('2d');
     
+class Compositor {
+        constructor(){
+         this.layers = [];  
+        }
+    
+    draw(context); {
+        this.layers.forEach(layer => {
+            layer(context);
+        });
+    }
+}
+
+function createBackgroundLayer(backgrounds, sprites) {
+    const buffer = document.createElement('canvas');
+    buffer.width = 256; //1500px
+    buffer.height = 240; //500px
+    
+    backgrounds.forEach(background => {
+        drawBackground(background, backgroundBuffer.getContext('2d'), sprites);
+      });
+    
+    return function drawBackgroundLayer(context) {
+      context.drawImage(backgroundBuffer, 0, 0);
+    };
+}
+
 Promise.all([
     loadDahlemSprite(),
     loadBackgroundSprites(),
     loadLevel('level1'),
     ])
 .then(([dahlemSprite, sprites, level]) => {
-    const backgroundBuffer = document.createElement('canvas');
-    backgroundBuffer.width = 1500;
-    backgroundBuffer.height = 500;
+    const comp = new Compositor();
     
-    level.backgrounds.forEach(background => {
-        drawBackground(background, backgroundBuffer.getContext('2d'), sprites);
-        });
+    const backgroundLayer = createBackgroundLayer(level.backgrounds, sprites);
+    comp.layers.push(backgroundLayer);
+    
     
     const pos = {
         x: 64,
@@ -35,7 +59,7 @@ Promise.all([
     };
     
     function update() {
-        context.drawImage(backgroundBuffer, 0, 0);
+        comp.draw(context);
         dahlemSprite.draw('idle', context, pos.x, pos.y);
         pos.x += 2;
         pos.y += 2;
