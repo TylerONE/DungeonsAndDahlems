b0e96025b1944454c306dfd95652028762bdd65f
TylerONE <yorukagekitsune@gmail.com>
2017-12-07 10:21:06 -0700

    Update sprites.js

--- gameBase/sprites.js
+++ gameBase/sprites.js
@@ -4,7 +4,7 @@ import SpriteSheet from './SpriteSheet.js';
 export function loadDahlemSprite() {
     return loadImage('gameBase/Character.png')
     .then(image => {
-        const sprites = new SpriteSheet(image, 64, 64);
+        const sprites = new SpriteSheet(image, 16, 16);
         sprites.define('idle', 259, 1, 16, 32);
         return sprites;
     });
@@ -13,7 +13,7 @@ export function loadDahlemSprite() {
 export function loadBackgroundSprites() {
     return loadImage('gameBase/tileset.png')
     .then(image => {
-        const sprites = new SpriteSheet(image, 32, 32);
+        const sprites = new SpriteSheet(image, 16, 16);
         sprites.defineTile('upperGround', 0, 0);
         sprites.defineTile('lowerGround', 0, 1);
         sprites.defineTile('sky', 2.92, 22.89);
