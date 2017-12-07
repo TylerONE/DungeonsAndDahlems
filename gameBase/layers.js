ed6212c066e5314d03f6599e121900c9bf0bde59
TylerONE <yorukagekitsune@gmail.com>
2017-12-07 10:02:41 -0700

    Update layers.js

--- gameBase/layers.js
+++ gameBase/layers.js
@@ -22,9 +22,11 @@ export function createBackgroundLayer(backgrounds, sprites) {
     };
 }
 
-export function createSpriteLayer(entity) {
+export function createSpriteLayer(entities) {
     return function drawSpriteLayer(context) {
+        entities.forEach(entity => {
         entity.draw(context);
+    });
     }
 }
 
