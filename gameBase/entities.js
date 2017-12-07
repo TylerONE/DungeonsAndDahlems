cae465af68ee096ac7615112814d87bac777a9cf
TylerONE <yorukagekitsune@gmail.com>
2017-12-06 22:53:47 -0700

    Update entities.js

--- gameBase/entities.js
+++ gameBase/entities.js
@@ -1,20 +1,28 @@
-import Entity from './Entity.js';
+import Entity, {Trait} from './Entity.js';
 import {loadDahlemSprite} from './sprites.js';
 
+class Velocity extends Trait {
+ constructor() {;
+   super('velocity');
+ }
+ 
+ update(entity, deltaTime) {
+        entity.pos.x += entity.vel.x * deltaTime;
+        entity.pos.y += entity.vel.y * deltaTime;
+ }
+}
 
 export function createDahlem() {
  return loadDahlemSprite()
  .then(sprite => {
     const dahlem = new Entity();
     
+    dahlem.addTrait(new Velocity());
+  
     dahlem.draw = function drawDahlem(context) {
         sprite.draw('idle', context, this.pos.x, this.pos.y);
     }
     
-    dahlem.update = function updateDahlem(deltaTime) {
-        this.pos.x += this.vel.x * deltaTime;
-        this.pos.y += this.vel.y * deltaTime;
-    }
     return dahlem;
   })
  }
