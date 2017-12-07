8376029bea4130f22337a43538dcee7772a57f1c
TylerONE <yorukagekitsune@gmail.com>
2017-12-06 22:03:53 -0700

    Update gameMechanics.js

--- gameMechanics.js
+++ gameMechanics.js
@@ -22,7 +22,7 @@ Promise.all([
     const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
     comp.layers.push(backgroundLayer);
    
-    const gravity = 800;
+    const gravity = 900;
     dahlem.pos.set(64, 384);
     dahlem.vel.set(200, -600);
     
