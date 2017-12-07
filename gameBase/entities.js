3a9ff6826dcdd659bdd3d94f1b128084b0b024c4
TylerONE <yorukagekitsune@gmail.com>
2017-12-06 21:25:30 -0700

    Update entities.js

--- gameBase/entities.js
+++ gameBase/entities.js
@@ -6,8 +6,6 @@ export function createDahlem() {
  return loadDahlemSprite()
  .then(sprite => {
     const dahlem = new Entity();
-    dahlem.pos.set(64, 180);
-    dahlem.vel.set(2, -10);
     
     dahlem.draw = function drawDahlem(context) {
         sprite.draw('idle', context, this.pos.x, this.pos.y);
