4d7312e91965583848ce925cd9fb1b4492af4dc4
TylerONE <yorukagekitsune@gmail.com>
2017-12-14 10:46:12 -0700

    Update gameMechanics.js

--- gameMechanics.js
+++ gameMechanics.js
@@ -20,7 +20,7 @@ Promise.all([
     dahlem.pos.set(64, -12000);
     
     createCollisionLayer(level);
-    drawCollisionLayer(context);
+    drawCollision(context);
     
     level.entities.add(dahlem);
     
