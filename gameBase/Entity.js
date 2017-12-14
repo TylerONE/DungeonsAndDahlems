f54f6310eb07a8c897678e77a2c3c913108c7d30
TylerONE <yorukagekitsune@gmail.com>
2017-12-14 11:08:39 -0700

    Update Entity.js

--- gameBase/Entity.js
+++ gameBase/Entity.js
@@ -12,8 +12,9 @@ export class Trait {
 
 export default class Entity {
     constructor() {
-     this.pos = new Vec2(0, 0); 
+        this.pos = new Vec2(0, 0); 
         this.vel = new Vec2(0, 0);
+        this.size = new Vec2(0, 0);
         
         this.traits = [];
     }
