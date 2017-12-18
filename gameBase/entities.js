7f5847b23db6a13587d84d8eaa9ed90260d5383a
TylerONE <yorukagekitsune@gmail.com>
2017-12-18 10:07:40 -0700

    Update entities.js

--- gameBase/entities.js
+++ gameBase/entities.js
@@ -8,7 +8,7 @@ export function createDahlem() {
  return loadDahlemSprite()
  .then(sprite => {
     const dahlem = new Entity();
-    dahlem.size.set(32, 32);
+    dahlem.size.set(16, 32);
   
     dahlem.addTrait(new Go());
     dahlem.addTrait(new Jump());
