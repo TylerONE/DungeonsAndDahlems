7b73631b476855a0e42958c1c052c6440c1daf8e
TylerONE <yorukagekitsune@gmail.com>
2017-12-18 10:16:51 -0700

    Create input.js

---
+++ gameBase/input.js
@@ -0,0 +1,24 @@
+export function setupKeyboard(entity) {   
+    const SPACE = 32;
+    
+    const input = new Keyboard();
+    
+    input.addMapping(SPACE, keyState => {
+        if (keyState) {
+            entity.jump.start();
+            console.log(dahlem.pos);
+        } else {
+            entity.jump.cancel(); 
+        }
+    });
+    
+        input.addMapping(39, keyState => {
+        entity.go.dir = keyState;
+    });
+    
+    input.addMapping(37, keyState => {
+        entity.go.dir = -keyState;
+    });
+    
+    return input;
+ }
