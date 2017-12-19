9907f7d9a09971885c5b944dd0aad43971324e3d
TylerONE <yorukagekitsune@gmail.com>
2017-12-19 10:06:47 -0700

    Update layers.js

--- gameBase/layers.js
+++ gameBase/layers.js
@@ -14,10 +14,22 @@ export function createBackgroundLayer(level, sprites) {
     };
 }
 
-export function createSpriteLayer(entities) {
-    return function drawSpriteLayer(context) {
+export function createSpriteLayer(entities, width = 128, height = 128) {
+    const spriteBuffer = document.createElement('canvas');
+    spriteBuffer.width = width;
+    spriteBuffer.height = height;
+    const spriteBufferContext = spriteBuffer.getContext('2d');
+    
+    return function drawSpriteLayer(context, camera) {
         entities.forEach(entity => {
-        entity.draw(context);
+        spriteBufferContext.clearRect(0, 0, width, height);
+            
+        entity.draw(spriteBufferContext);
+            
+        context.drawImage(
+            spriteBuffer,
+            entity.pos.x - camera.pos.x,
+            entity.pos.y - camera.pos.y);
     });
     }
 }
