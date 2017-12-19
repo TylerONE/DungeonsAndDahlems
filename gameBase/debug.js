b38a2b732ceca9ca81ddf52fe8ecf6c3862a99bf
TylerONE <yorukagekitsune@gmail.com>
2017-12-19 13:29:21 -0700

    Update debug.js

--- gameBase/debug.js
+++ gameBase/debug.js
@@ -1,7 +1,6 @@
 export function setupMouseControl(canvas, entity, camera) {
 ['mousedown', 'mousemove'].forEach(eventName => {
          canvas.addEventListener(eventName, event => {
-        
              if (event.buttons === 1) {
               entity.vel.set(0, 0);
               entity.pos.set(
